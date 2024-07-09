6616d6c0f99e8ae83ab4363e50f1c0635b917805
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-07-08 23:52:26 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+game 'rdr3'
+
+author 'TheLux'
+description 'A simple resource to open or close all interiors in RedM using QBCore'
+version '1.0.0'
+
+client_script {
+    'config.lua',
+    'client.lua'
+}
